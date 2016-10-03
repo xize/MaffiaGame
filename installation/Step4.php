91232bce487cca250aff189dc962490f5317918c
xize <glucassen@live.nl>
2016-10-03 20:49:13 +0200

    finished connection.php write for the installation api

--- installation/Step4.php
+++ installation/Step4.php
@@ -9,11 +9,15 @@ class Step4 implements Step {
     public function createConnectionFile() {
         $file = fopen("../includes/connection/connection.php", "w") or die("unable to save file!");
 
-        $networkl = "define(\"network\", \"". $_POST['network'] ."\");\n";
-        $userl = "define(\"user\", \"". $_POST['user'] ."\");\n";
-        $passwordl = "define(\"password\", \"". $_POST['password'] ."\");\n";
-        $databasel = "define(\"database\", \"". $_POST['database'] ."\");\n";
+        $networkl = "define(\"NETWORK\", \"". $_POST['network'] ."\");\n";
+        $userl = "define(\"USER\", \"". $_POST['user'] ."\");\n";
+        $passwordl = "define(\"PASSWORD\", \"". $_POST['password'] ."\");\n";
+        $databasel = "define(\"DATABASE\", \"". $_POST['database'] ."\");\n";
 
-        $connection = "<?php\n\n".$networkl."".userl."".$passwordl."".$databasel."\n";
+        $connection = "<?php\n\n".$networkl."".userl."".$passwordl."".$databasel."\nmysqli_connect(NETWORK, USER, PASSWORD) && mysqli_select_db(DATABASE) or die(\"<span class=\"sqlerror\"/>cannot connect to the database!, please try again later</span>\");";
+
+        fwrite($file,  $connection);
+        fclose($file);
+
     }
 }
