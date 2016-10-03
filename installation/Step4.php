6af3427e0a7ff61f364a431a70900b2d44d344cb
xize <glucassen@live.nl>
2016-10-03 21:22:49 +0200

    rewrite step4 inside the installation api

--- installation/Step4.php
+++ installation/Step4.php
@@ -3,18 +3,42 @@ class Step4 implements Step {
 
     public function showStep()
     {
+        $this->createConnectionFile();
+
+    }
+
+    public function createTables() {
+        include "../includes/connection/connection.php";
 
     }
 
     public function createConnectionFile() {
         $file = fopen("../includes/connection/connection.php", "w") or die("unable to save file!");
 
-        $networkl = "define(\"NETWORK\", \"". $_POST['network'] ."\");\n";
-        $userl = "define(\"USER\", \"". $_POST['user'] ."\");\n";
-        $passwordl = "define(\"PASSWORD\", \"". $_POST['password'] ."\");\n";
-        $databasel = "define(\"DATABASE\", \"". $_POST['database'] ."\");\n";
-
-        $connection = "<?php\n\n".$networkl."".userl."".$passwordl."".$databasel."\nmysqli_connect(NETWORK, USER, PASSWORD) && mysqli_select_db(DATABASE) or die(\"<span class=\"sqlerror\"/>cannot connect to the database!, please try again later</span>\");";
+        $connection = "<?php\n";
+        $connection .= "define(\"NETWORK\", ".$_POST['network'].");\n";
+        $connection .= "define(\"USER\", ".$_POST['user'].");\n";
+        $connection .= "define(\"PASSWORD\", ".$_POST['password']."\n);";
+        $connection .= "define(\"DATABASE\", ".$_POST['db']."\n);";
+        $connection .= "\n\n";
+        $connection .= "class Connection {\n";
+        $connection .= "{";
+        $connection .= "\n";
+        $connection .= '    private static $con;\n';
+        $connection .= '    private $sql;\n\n';
+        $connection .= "    public function __construct() {\n";
+        $connection .= '        $this->sql = new mysqli(NETWORK, USER, PASSWORD, DATABASE);\n';
+        $connection .= "    }\n\n";
+        $connection .= "    public function getConnection() {\n";
+        $connection .= "        return $this->sql;\n";
+        $connection .= "    }\n\n";
+        $connection .= "    public static function getMysqlServer() {\n";
+        $connection .= "        if(SELF::con != null) {\n";
+        $connection .= '            $con = new Connection();\n';
+        $connection .= "        }\n";
+        $connection .= '        return $con;\n';
+        $connection .= "    }\n\n";
+        $connection .= "}";
 
         fwrite($file,  $connection);
         fclose($file);
